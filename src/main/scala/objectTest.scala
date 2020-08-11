c0430e3d2abcc7be006b8a0da549f633bf2937cd
Ulyses Rico <ulyses@quart.com.mx>
2020-08-11 10:28:00 -0500

    ...

--- src/main/scala/objectTest.scala
+++ src/main/scala/objectTest.scala
@@ -4,10 +4,6 @@ import com.datastax.oss.driver.api.core.CqlSessionBuilder
 import com.datastax.oss.driver.api.core.cql.{Row, SimpleStatementBuilder}
 import java.util.function.Consumer
 
-import com.datastax.oss.driver.api.core.data.CqlDuration
-
-
-
 object objectTest extends App{
 
   var session = new CqlSessionBuilder()
@@ -16,24 +12,33 @@ object objectTest extends App{
     .withKeyspace("thesis")
     .build()
 
-  var query="select id_thesis, period_number from thesis.tbthesis where period_number=10"
+  var query="select id_thesis, period_number from thesis.tbthesis where period_number>4 ALLOW FILTERING"
 
+  //With 2 mins of TimeOut I got the 5th period complete
+  //With 1 min of Time Out I got the whole database
   var st= new SimpleStatementBuilder(query)
     .setIdempotence(true)
     .setPageSize(1000)
-    .setTimeout(Duration.ofSeconds(1000))
+    .setTimeout(Duration.ofMinutes(1))
     .build()
 
+
+  var contar:Int=0
 
   session.execute(st).forEach(new Consumer[Row] (){
 
     override def accept(row: Row): Unit = {
 
+      contar+=1
       println(row.getFormattedContents())
+      println(contar.toString)
+
+
+
     }
   })
 
-  println("Done")
+  println("DONE")
 }
 
 
