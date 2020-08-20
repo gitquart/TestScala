ab3000258d356c98968447ed78f0629221681781
Ulyses Rico <ulyses@quart.com.mx>
2020-08-20 17:52:52 -0500

    ...

--- src/main/scala/objectTest.scala
+++ src/main/scala/objectTest.scala
@@ -3,29 +3,29 @@ import java.time.Duration
 import java.util.function.Consumer
 import com.datastax.oss.driver.api.core.CqlSessionBuilder
 import com.datastax.oss.driver.api.core.cql.{Row, SimpleStatementBuilder}
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
 import java.util.Calendar
+import com.datastax.spark.connector._
+
+
 
 object objectTest extends App{
 
+  val currentDirectory:String = System.getProperty("user.dir")
+
+
   //Spark
 /*
-  val conf = new SparkConf()
-  conf.setMaster("local")
-      .setAppName("appSparkCassandra")
-
-  val sc= new SparkContext(conf)
-
-  val lst:List[String]= List("hello","there")
-  val lst2:List[String]=List("hhh","popopo")
-  val rdd1= sc.makeRDD(lst)
-  val rdd2= sc.makeRDD(lst2)
+  val conf = new SparkConf(true)
+    .set("spark.cassandra.connection.host", "127.0.0.1")
+  val sc = new SparkContext("dse://127.0.0.1:7077", "test", conf)
+  val rdd = sc.cassandraTable("my_keyspace", "my_table")
 
 
  */
 
-  val currentDirectory:String = System.getProperty("user.dir")
+
+  /*
+  Datastax connection only
   var session = new CqlSessionBuilder()
     .withAuthCredentials("test", "testquart")
     .withCloudSecureConnectBundle(Paths.get(currentDirectory+"\\secure-connect-dbtest.zip"))
@@ -58,6 +58,10 @@ object objectTest extends App{
   println(contar.toString)
   print("End:"+Calendar.getInstance.getTime.toString)
   println("DONE")
+
+   */
+
+
 }
 
 
